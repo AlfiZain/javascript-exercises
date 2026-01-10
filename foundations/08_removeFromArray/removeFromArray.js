680d4b82284a80b74a51d4b2a6cfbfc264c9a5fc
alfizain <116138256+AlfiZain@users.noreply.github.com>
2026-01-10 14:47:58 +0700

    complete exercise foundations/6-11

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (sourceArray, ...valueToRemove) {
+  return sourceArray.filter((value) => !valueToRemove.includes(value));
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+  if (min < 0 || max < 0) return 'ERROR';
+  if (min > max) [max, min] = [min, max];
 
+  let total = 0;
+  for (min; min <= max; min++) {
+    total += min;
+  }
+  return total;
 };
 
 // Do not edit below this line
